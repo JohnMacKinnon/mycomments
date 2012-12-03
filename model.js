a2cc4fdc7c5f6e29dbbd1d52ce27d6fcac94862a
JohnMacKinon <tq2310@gmail.com>
2012-12-02 21:02:32 -0500

    4commit

--- model.js
+++ model.js
@@ -1,11 +1,11 @@
 // Commenters -- data model
-// Loaded on both the client and the server
+
 
 
 Comments = new Meteor.Collection("comments");
 
 Meteor.methods({
-  // options should include: title, description, x, y, public
+
   addComment: function (options) {
     options = options || {};
 
@@ -22,7 +22,7 @@ Meteor.methods({
   
 });
 
-var url = "http://www.411.fm";
+var url = "http://www.somedomain.com";
 var user_id = this.userId;
 
 	
@@ -31,14 +31,13 @@ var email = function () {
  var user = Meteor.user();
     if (!user)
       return 'NoUser';
-
     if (user.profile && user.profile.name)
       return user.profile.name;
     if (user.username)
       return user.username;
     if (user.emails && user.emails[0] && user.emails[0].address)
       return user.emails[0].address;
-	console.log(user);
+
     return  user._id;
 };
 
